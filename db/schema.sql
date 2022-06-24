62fb5d83b0a7fd9cc9894accfab7f2357479d9d7
Eyesack <isaac.laflamme@gmail.com>
2022-06-24 14:38:57 -0700

    database set up with all functionality neccesary

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
+
+USE staff_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+        REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+        References employees(id)
+);
+
+SOURCE ./seeds.sql;
---
+++ db/seeds.sql
@@ -0,0 +1,37 @@
+INSERT INTO departments (name)
+    VALUES ('Administration'),
+           ('Development'),
+           ('Sales')
+;
+
+INSERT INTO roles (title, salary, department_id)
+    VALUES ('Manager', 110000.00, 1),
+           ('Engineer', 80000.00, 2),
+           ('Intern', 40000.00, 2)
+;
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    VALUES ('Katlynn', 'Orchard', 1, null),
+           ('Isaac', 'LaFlamme', 2, 1)
+;
+
+SELECT * FROM departments;
+
+SELECT * FROM roles;
+
+SELECT * FROM employees;
+
+SELECT r.title AS 'Title', concat('$ ', r.salary) AS 'Salary', d.name AS 'Department'
+FROM roles AS r
+JOIN departments AS d
+ON r.department_id = d.id;
+
+SELECT r.title AS 'Title', concat('$ ', r.salary) AS 'Salary', d.name AS 'Department'
+FROM roles AS r
+JOIN departments AS d
+ON r.department_id = d.id;
+
+SELECT concat(e1.first_name, ' ',e1.last_name) AS 'Name', concat(e2.first_name, ' ',e2.last_name) AS 'Manager Name'
+FROM employees AS e1
+JOIN employees AS e2
+ON e1.manager_id = e2.id
